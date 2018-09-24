eaca38626350353569173500dee7c142725b7a9e
brianyu18 <brianyu1990@gmail.com>
2018-09-24 01:09:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list_names)
+  new_list = []
+  list_names.each do |make|
+    new_list << badge_maker(make)
+  end
+  puts new_list
+  return new_list
+end
+
+def assign_rooms(list_names)
+  rooms = []
+  list_names.each_with_index do |name, idx|
+    rooms << "Hello, #{name}! You'll be assigned to room #{idx+1}!"
+  end
+  puts rooms
+  rooms
+end
+
+def printer(names)
+  batch_badge_creator(names)
+  assign_rooms(names)
+end
